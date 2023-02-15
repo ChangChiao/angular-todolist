3f14f879418dd72ad7d9220eee9a09ca8195af6d
joe <joe.chang1014@gmail.com>
2023-02-15 19:45:10 +0800

    feat tailwind css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DoneComponent } from './done/done.component';
+import { HeaderComponent } from './header/header.component';
+import { IndexComponent } from './index/index.component';
+import { PlanComponent } from './plan/plan.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DoneComponent,
+    HeaderComponent,
+    IndexComponent,
+    PlanComponent
   ],
   imports: [
     BrowserModule,
